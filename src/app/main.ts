d42f3d08195ed3fe6e98a6088b219eb80141cde6
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-10-11 19:15:13 -0600

    Start of CRUD project, used 'faker' library to generate test date for new products

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,16 +1,29 @@
 // En este archivo se ejecuta la función addProduct() que esta definida en el archivo product.service.ts, esta función recibe un objeto con la estructura de un producto, y lo agrega al array de productos.
-import { addProduct } from "./products/product.service";
 
-addProduct({
-  id:  '1',
-  title: 'Product 1',
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  stock: 90,
-  category: {
-    id: '12',
-    name: 'Category 1',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  }
-})
+import { addProduct, products } from "./products/product.service";           // Importamos la función addProduct() y el array de productos desde el archivo product.service.ts.
+import { faker } from '@faker-js/faker';                                     // Importamos la librería 'faker' para generar datos aleatorios de los productos para hacer pruebas.
+
+for (let index = 0; index < 50; index++) {                                   // Definimos un ciclo 'for' para ejecutar la función addProduct() 50 veces.
+  addProduct({                                                               // Ejecuto la función addProduct() que esta definida en el archivo product.service.ts, esta función recibe un objeto con la estructura de un producto, y lo agrega al array de productos.
+    id: faker.string.uuid(),                                                 // Para agregar los datos de los productos, usamos la librería 'faker' para generar datos aleatorios de los productos para hacer pruebas.
+    description: faker.commerce.productDescription(),                        // La librería 'faker' tiene contextos para generar datos aleatorios de los productos, como: nombre, descripción, precio, etc.
+    image: faker.image.url(),
+    color: faker.color.human(),
+    size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),                 // Para agregar un Array de datos aleatorios dentro de unas opciones especificadas, usamos el método faker.helpers.arrayElement() de la librería 'faker'.
+    price: parseInt(faker.commerce.price(), 10),                             // Podemos 'parsear' el precio a un número entero usando el método parseInt() de JavaScript, y especificando la base con el parámetro '10'.
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.arrayElements([]),
+    title: faker.commerce.productName(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    stock: faker.number.int({ min: 10, max: 100 }),                          // Podemos generar rangos de números aleatorios con la librería 'faker' usando el método faker.number.int() y especificando el rango con los parámetros 'min' y 'max'.
+    category: {                                                              // Agregamos un objeto con la estructura de una categoría siempre usando la librería 'faker' para generar datos aleatorios de las categorías para hacer pruebas.
+      id: faker.string.uuid(),
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    }
+  })
+}
+
+console.log(products);
--- src/app/products/product.model.ts
+++ src/app/products/product.model.ts
@@ -1,13 +1,19 @@
 // En este archivo se define la interfaz ProductInterface, que define la estructura de un producto, y un 'type' Sizes, que define los tamaños de los productos. Estos dos elementos se importan en otros archivos para definir la estructura de los productos y para definir el tipo de datos de los productos.
 
-import { Category } from "../categories/category.model";
+import { Category } from "../categories/category.model";            // Importamos la interfaz Category que esta definida en el archivo category.model.ts, esta Category tiene la estructura de un objeto.
 import { BaseModel } from "../base.model";                          // Importamos el 'BaseModel' que esta definido en el archivo base.model.ts. y aplicamos Herencia para que esta interfaz ProductInterface herede las propiedades de la interfaz BaseModel.
 
 export type Sizes = 'S' | 'M' | 'L' | 'XL';
 
 export interface ProductInterface extends BaseModel {               // Interfaz que define la estructura de un producto. Aplicamos Herencia para que esta interfaz ProductInterface herede las propiedades de la interfaz BaseModel. Las propiedades de la interfaz BaseModel son: id, createdAt, updatedAt.
   title: string,
+  image: string,
+  description: string,
   stock: number,
+  color: string,
+  price: number,
+  isNew: boolean,
+  tags: string[]
+  category: Category,                                               // Acá usamos la interfaz Category que esta definida en el archivo category.model.ts, esta Category tiene la estructura de un objeto.
   size?: Sizes,
-  category: Category,                                   // Acá usamos la interfaz Category que esta definida en el archivo category.model.ts, esta Category tiene la estructura de un objeto.
 }
